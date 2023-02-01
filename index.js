cfc85f9aa3a1403e6365f669e41a81620675230b
jaya.jc999@gmail.com <jaya.jc999@gmail.com>
2023-02-01 20:00:30 +0545

    new

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5001;
+const app = express();
+
+
+//Enable body parser...
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+app.listen(port, () => console.log(`server with ${port}`));
